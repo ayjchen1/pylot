58e725eaa242151d7516c2c7cf915ee1c3c497f6
Alice Chen <chenalice2020@gmail.com>
2021-02-08 09:40:04 -0600

    data processing

--- visnn/model.py
+++ visnn/model.py
@@ -1,66 +1,94 @@
 import torch
 import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 
 import matplotlib.pylot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import sklearn
+from sklearn.model_selection import train_test_split
 
 torch.manual_seed(1)    # reproducible
-BATCH_SIZE = 64
-NUMEPOCH = 50
+BATCH_SIZE = 10
+EPOCHS = 50
+LEARNING_RATE = 0.005
+MOMENTUM = 0.9
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class VisDataset(Dataset):
+    """
+    Pytorch dataset for the visibility uncertainty values
+
+        X: relative position of obstacles
+        y: the visibility error
+    """
+    def __init__(self, X_data, y_data):
+        self.X_data = X_data
+        self.y_data = y_data
+
+    def __getitem__(self, idx):
+        return self.X_data[idx], self.y_data[idx]
+
+    def __len__(self):
+        return len(self.X_data)
 
 def read_data(filename):
     """
     Reads the visibility data (in csv format) from the filename
     inside vis_data folder
 
-    Returns a pandas dataframe ?"
+    Returns a pandas dataframe split into train (70) and test (30)
+
+    normalize inputs? or no?
 
     """
-    return []
+    df = pd.read_csv(filename)
 
+    X = df.iloc[:, -4:-1]
+    y = df.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
 
-def process_data(vis_data):
-    """
-    Processes the data by stripping away extra info and leaving:
-        X: the (x, y, z) relative location of the obstacle to the ego vehicle
-        y: the visibility error/uncertainty
-    """
-    return 0
+    X_train, X_test, y_train, y_test = X_train.astype(float), X_test.astype(float),
+                                        y_train.astype(float), y_test.astype(float)
 
+    return X_train, X_test, y_train, y_test
+
+def get_datasets(X_train, X_test, y_train, y_test):
+    train_dataset = VisDataset(torch.from_numpy(X_train).float(),
+                                torch.from_numpy(y_train).float())
+    test_dataset = VisDataset(torch.from_numpy(X_test).float(),
+                                torch.from_numpy(y_test).float())
+
+    return train_dataset, test_dataset
+
+def get_dataloader(dataset, batch_size):
+    loader = DataLoader(dataset=dataset,
+                        batch_size=batch_size,
+                        num_workers=2)
+    return loader
 
 def print_stats(epoch, step, loss_sum):
     loss_str = "EPOCH: {:2d}, STEP: {:5d}, LOSS: {:.4f}".format(epoch, step, loss_sum)
     print(loss_str)
 
-def run_nn():
-    x, y = get_dataset() # implement this!
+def run_nn(loader):
 
-    """
-    torch_dataset = Data.TensorDataset(x, y)
-
-    loader = Data.DataLoader(
-        dataset=torch_dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=True, num_workers=2,)
-    """
-
-    # torch can only train on Variable, so convert them to Variable
-    x, y = Variable(x), Variable(y)
-
+    # define neural net architecture
     net = torch.nn.Sequential(
             torch.nn.Linear(3, 100),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(100, 50),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(50, 1),
-        )
+        ).to(device)
 
     criterion = torch.nn.MSELoss() # (y - yhat)^2
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    optimizer = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 
     # train the neural net
-    for epoch in range(NUMEPOCH):
+    for epoch in range(EPOCHS):
         loss_sum = 0
 
         for step, data in enumerate(loader):
@@ -81,7 +109,11 @@ def run_nn():
 
 
 if __name__=="__main__":
-    run_nn()
+    X_train, X_test, y_train, y_test = read_data("../vis_data/vis.csv")
+    train_dataset, test_dataset = get_datasets(X_train, X_test, y_train, y_test)
+    train_loader = get_dataloader(train_dataset, BATCH_SIZE)
+
+    run_nn(train_loader)
 
 
 
